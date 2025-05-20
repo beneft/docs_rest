556371a663544ea66e494592bd9f65adc209f5c7
PollyBreak <mspolinko@gmail.com>
2025-05-20 17:40:00 +0500

    added approval

---
+++ signature-service/src/main/java/com/project/signatureservice/controller/ApprovalController.java
@@ -0,0 +1,57 @@
+package com.project.signatureservice.controller;
+
+import com.example.commondto.DocumentMetadataDTO;
+import com.example.commondto.SignerDTO;
+import com.project.signatureservice.model.Signature;
+import com.project.signatureservice.model.SigningProcess;
+import com.project.signatureservice.service.ApprovalService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/approval")
+@RequiredArgsConstructor
+public class ApprovalController {
+
+    private final ApprovalService approvalService;
+
+    @PostMapping("/start")
+    public ResponseEntity<String> startSigningProcess(@RequestBody SigningProcess signingProcess) {
+        approvalService.startSigningProcess(signingProcess);
+        return ResponseEntity.ok("Signing process started.");
+    }
+
+    @GetMapping("/{documentId}/signers")
+    public ResponseEntity<List<SignerDTO>> getSigners(@PathVariable String documentId) {
+        return ResponseEntity.ok(approvalService.getSigners(documentId));
+    }
+
+    @GetMapping("/{documentId}/can-sign/{userId}")
+    public ResponseEntity<Boolean> canSign(
+            @PathVariable String documentId,
+            @PathVariable Long userId) {
+        return ResponseEntity.ok(approvalService.canSign(documentId, userId));
+    }
+
+    @PostMapping("/sign")
+    public ResponseEntity<String> sign(@Valid @RequestBody Signature signature) {
+        approvalService.applySignature(signature);
+        return ResponseEntity.ok("Signature recorded.");
+    }
+
+    @GetMapping("/{documentId}/status")
+    public ResponseEntity<DocumentMetadataDTO> getDocumentStatus(@PathVariable String documentId) {
+        return ResponseEntity.ok(approvalService.getDocumentWithSigners(documentId));
+    }
+
+    @PostMapping("/{documentId}/decline/{userId}")
+    public ResponseEntity<String> declineSignature(@PathVariable String documentId, @PathVariable Long userId) {
+        approvalService.declineSignature(documentId, userId);
+        return ResponseEntity.ok("Signature declined.");
+    }
+
+}
