b89cb5fc4f131524b46e5798105bc90c9e2a2899
PollyBreak <mspolinko@gmail.com>
2025-05-22 12:58:34 +0500

    security upd

--- filestorage-service/src/main/java/com/project/config/SecurityConfig.java
+++ filestorage-service/src/main/java/com/project/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,29 +15,43 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
+import static org.springframework.web.servlet.function.RequestPredicates.headers;
+
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filter(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/documents/author/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/documents/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/documents").authenticated()
-                        .requestMatchers(HttpMethod.GET, "/documents/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE,"/documents/**").authenticated()
+                        .requestMatchers(HttpMethod.POST,"/documents").authenticated()
+                        .requestMatchers(HttpMethod.GET,"/documents/**").permitAll()
                         .anyRequest().permitAll())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .headers(headers -> {
+                    headers.frameOptions(frame -> frame.disable());
+                    headers.contentSecurityPolicy(csp -> csp.policyDirectives(
+                            "frame-ancestors 'self' http://localhost:3000"));
+                });
+
         return http.build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
-        cors.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3004"));
+        cors.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:3004"
+        ));
         cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         cors.setAllowedHeaders(List.of("*"));
         cors.setAllowCredentials(true);
